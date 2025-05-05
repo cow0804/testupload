b891f802240f1a72d43fe27ee8d43d8830c3adfc
cow0804 <lboite65@gmail.com>
2025-05-05 10:02:51 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+// data.js
+let flightDataArray = [
+    // 格式：(flight, type, section, category, details)
+    ["907", "32Q", "頭車", "長車", { kitchen: 1, bar: 0, elderly: 0, water: 1, total: 2 }],
+    ["907", "32Q", "頭車", "短車", { kitchen: 0, bar: 1, elderly: 0, water: 1, total: 2 }],
+    ["907", "32Q", "頭車", "焗爐", { kitchen: 2, bar: 0, elderly: 0, water: 0, total: 2 }],
+    ["907", "32Q", "頭車", "手挽", { kitchen: 0, bar: 0, elderly: 2, water: 1, total: 2 }],
+    ["907", "32Q", "中車", "長車", { kitchen: 1, bar: 0, elderly: 0, water: 1, total: 2 }],
+    ["907", "32Q", "中車", "短車", { kitchen: 0, bar: 1, elderly: 0, water: 1, total: 2 }],
+    ["907", "32Q", "中車", "焗爐", { kitchen: 2, bar: 0, elderly: 0, water: 0, total: 2 }],
+    ["907", "32Q", "中車", "手挽", { kitchen: 0, bar: 0, elderly: 2, water: 1, total: 2 }],
+    ["907", "32Q", "尾車", "長車", { kitchen: 5, bar: 1, elderly: 0, water: 0, total: 6 }],
+    ["907", "32Q", "尾車", "短車", { kitchen: 1, bar: 1, elderly: 0, water: 0, total: 2 }],
+    ["907", "32Q", "尾車", "焗爐", { kitchen: 4, bar: 0, elderly: 0, water: 0, total: 4 }],
+    ["907", "32Q", "尾車", "手挽", { kitchen: 1, bar: 0, elderly: 6, water: 6, total: 12 }],
+    // ... 其他資料（請參考之前回應的完整資料）
+];
